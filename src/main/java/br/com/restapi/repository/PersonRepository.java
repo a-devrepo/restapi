e05cbc34069d46d892e9702ca4c9f4ee8a2fc3e1
alison <adevpr11@gmail.com>
2023-12-20 18:18:09 -0300

    Conexão com banco de dados e mapeamento da classe Person

---
+++ src/main/java/br/com/restapi/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.restapi.repository;
+
+import br.com.restapi.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person,Long> {
+}
