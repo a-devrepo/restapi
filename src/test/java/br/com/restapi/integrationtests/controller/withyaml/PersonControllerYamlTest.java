44b799375942c3179329967ce2f9f1a3771f2611
alison <adevpr11@gmail.com>
2024-02-01 15:43:31 -0300

    Testes de integração do PersonController com YML

---
+++ src/test/java/br/com/restapi/integrationtests/controller/withyaml/PersonControllerYamlTest.java
@@ -0,0 +1,291 @@
+package br.com.restapi.integrationtests.controller.withyaml;
+
+import br.com.restapi.configs.TestConfigs;
+import br.com.restapi.integrationtests.controller.withyaml.mapper.YamlMapper;
+import br.com.restapi.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.restapi.integrationtests.vo.AccountCredentialsVO;
+import br.com.restapi.integrationtests.vo.PersonVO;
+import br.com.restapi.integrationtests.vo.TokenVO;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PersonControllerYamlTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static YamlMapper objectMapper;
+    private static PersonVO personVO;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new YamlMapper();
+        personVO = new PersonVO();
+    }
+
+    @Test
+    @Order(0)
+    void authorization() throws IOException {
+        AccountCredentialsVO user = new AccountCredentialsVO("alison", "admin1234");
+
+        String accessToken = given()
+                .config(RestAssuredConfig
+                        .config()
+                        .encoderConfig(EncoderConfig
+                                .encoderConfig()
+                                .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YML, ContentType.TEXT)))
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(TestConfigs.CONTENT_TYPE_YML)
+                .accept(TestConfigs.CONTENT_TYPE_YML)
+                .body(user,objectMapper)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenVO.class,objectMapper)
+                .getAccessToken();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+    }
+
+    @Test
+    @Order(1)
+    void testCreate() throws IOException {
+        mockPerson();
+
+        var createdPerson =
+                given()
+                        .spec(specification)
+                        .config(RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig
+                                        .encoderConfig()
+                                        .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YML, ContentType.TEXT)))
+                        .contentType(TestConfigs.CONTENT_TYPE_YML)
+                        .accept(TestConfigs.CONTENT_TYPE_YML)
+                        .body(personVO,objectMapper)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(PersonVO.class,objectMapper);
+
+        personVO = createdPerson;
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+
+        assertTrue(createdPerson.getId() > 0);
+
+        assertEquals("Richard",createdPerson.getFirstName());
+        assertEquals("Stallman",createdPerson.getLastName());
+        assertEquals("New York City, New York, US",createdPerson.getAddress());
+        assertEquals("M",createdPerson.getGender());
+    }
+
+    @Test
+    @Order(2)
+    void testUpdate() throws IOException {
+        personVO.setLastName("Stallman III");
+
+        var createdPerson =
+                given()
+                        .spec(specification)
+                        .config(RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig
+                                        .encoderConfig()
+                                        .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YML, ContentType.TEXT)))
+                        .contentType(TestConfigs.CONTENT_TYPE_YML)
+                        .accept(TestConfigs.CONTENT_TYPE_YML)
+                        .body(personVO,objectMapper)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(PersonVO.class,objectMapper);
+
+        personVO = createdPerson;
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getId());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+
+        assertEquals(personVO.getId(), createdPerson.getId());
+
+        assertEquals("Richard",createdPerson.getFirstName());
+        assertEquals("Stallman III",createdPerson.getLastName());
+        assertEquals("New York City, New York, US",createdPerson.getAddress());
+        assertEquals("M",createdPerson.getGender());
+    }
+
+    @Test
+    @Order(3)
+    void testFindById() throws IOException {
+        mockPerson();
+
+        var persistedPerson =
+                given()
+                        .spec(specification)
+                        .config(RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig
+                                        .encoderConfig()
+                                        .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YML, ContentType.TEXT)))
+                        .contentType(TestConfigs.CONTENT_TYPE_YML)
+                        .accept(TestConfigs.CONTENT_TYPE_YML)
+                        .pathParam("id",personVO.getId())
+                        .when()
+                        .get("{id}")
+                        .then()
+                        .extract()
+                        .body()
+                        .as(PersonVO.class,objectMapper);
+
+        personVO = persistedPerson;
+
+        assertNotNull(persistedPerson);
+        assertNotNull(persistedPerson.getId());
+        assertNotNull(persistedPerson.getFirstName());
+        assertNotNull(persistedPerson.getLastName());
+        assertNotNull(persistedPerson.getAddress());
+        assertNotNull(persistedPerson.getGender());
+
+        assertEquals(personVO.getId(), persistedPerson.getId());
+
+        assertEquals("Richard",persistedPerson.getFirstName());
+        assertEquals("Stallman III",persistedPerson.getLastName());
+        assertEquals("New York City, New York, US",persistedPerson.getAddress());
+        assertEquals("M",persistedPerson.getGender());
+    }
+
+    @Test
+    @Order(4)
+    void testDelete() throws IOException {
+        mockPerson();
+
+                given()
+                        .spec(specification)
+                        .config(RestAssuredConfig
+                                .config()
+                                .encoderConfig(EncoderConfig
+                                        .encoderConfig()
+                                        .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YML, ContentType.TEXT)))
+                        .contentType(TestConfigs.CONTENT_TYPE_YML)
+                        .accept(TestConfigs.CONTENT_TYPE_YML)
+                        .pathParam("id",personVO.getId())
+                        .when()
+                        .delete("{id}")
+                        .then()
+                        .statusCode(204);
+    }
+
+    @Test
+    @Order(5)
+    void testFindAll() throws IOException {
+
+        var content =
+                given()
+                        .spec(specification)
+                        .config(
+                                RestAssuredConfig
+                                        .config()
+                                        .encoderConfig(EncoderConfig.encoderConfig()
+                                                .encodeContentTypeAs(
+                                                        TestConfigs.CONTENT_TYPE_YML,
+                                                        ContentType.TEXT)))
+                        .contentType(TestConfigs.CONTENT_TYPE_YML)
+                        .accept(TestConfigs.CONTENT_TYPE_YML)
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(PersonVO[].class,objectMapper);
+
+
+        List<PersonVO> people = Arrays.asList(content);
+
+        PersonVO foundPersonOne = people.get(0);
+
+        assertNotNull(foundPersonOne.getId());
+        assertNotNull(foundPersonOne.getFirstName());
+        assertNotNull(foundPersonOne.getLastName());
+        assertNotNull(foundPersonOne.getAddress());
+        assertNotNull(foundPersonOne.getGender());
+
+        assertTrue(foundPersonOne.getId() > 0);
+
+        assertEquals("Nikola",foundPersonOne.getFirstName());
+        assertEquals("Tesla",foundPersonOne.getLastName());
+        assertEquals("Croatia",foundPersonOne.getAddress());
+        assertEquals("Male",foundPersonOne.getGender());
+    }
+
+    @Test
+    @Order(6)
+    void testFindAllWithoutToken() throws IOException {
+
+       RequestSpecification specificationWithoutToken = new RequestSpecBuilder()
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+                given()
+                        .spec(specificationWithoutToken)
+                        .contentType(TestConfigs.CONTENT_TYPE_YML)
+                        .accept(TestConfigs.CONTENT_TYPE_YML)
+                        .when()
+                        .get()
+                        .then()
+                        .statusCode(403)
+                        .extract()
+                        .body()
+                        .asString();
+    }
+
+    private void mockPerson() {
+        personVO.setFirstName("Richard");
+        personVO.setLastName("Stallman");
+        personVO.setAddress("New York City, New York, US");
+        personVO.setGender("M");
+    }
+}
