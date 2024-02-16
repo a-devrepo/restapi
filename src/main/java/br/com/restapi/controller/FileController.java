555f94839b159f95bf4f72d2b3752a88dc7a09c5
alison <adevpr11@gmail.com>
2024-02-16 17:59:36 -0300

    Criando o Controller responsável pelo upload e download de arquivos

---
+++ src/main/java/br/com/restapi/controller/FileController.java
@@ -0,0 +1,39 @@
+package br.com.restapi.controller;
+
+import br.com.restapi.service.FileStorageService;
+import br.com.restapi.vo.v1.UploadFileResponseVO;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.logging.Logger;
+
+@Tag(name = "File endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    private Logger logger = Logger.getLogger(FileController.class.getName());
+
+    private FileStorageService service;
+
+    public FileController(FileStorageService fileStorageService) {
+        this.service = fileStorageService;
+    }
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseVO uploadFile(@RequestParam("file")MultipartFile file) {
+        logger.info("Storing file to disk");
+
+        var fileName = service.storageFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(fileName)
+                .toUriString();
+        return new UploadFileResponseVO(fileName,fileDownloadUri,file.getContentType(),file.getSize());
+    }
+}
